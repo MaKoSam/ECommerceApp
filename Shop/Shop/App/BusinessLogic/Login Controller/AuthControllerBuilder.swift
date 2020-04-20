e425b060fd737abbad6954511b56afe3228f7ccd
MaKoSam <mazniker@icloud.com>
2020-04-20 17:32:24 +0800

    Authorizator - класс, отвтетственный за выполнение авторизации пользователя

---
+++ Shop/Shop/App/BusinessLogic/Login Controller/AuthControllerBuilder.swift
@@ -0,0 +1,80 @@
+//
+//  AuthControllerBuilder.swift
+//  Shop
+//
+//  Created by Sam Mazniker on 19/04/2020.
+//  Copyright © 2020 Sam Mazniker. All rights reserved.
+//
+
+import UIKit
+
+class AuthControllerBuilder: NSObject {
+    func buildDefaultController() -> AuthViewController{
+        var controller = AuthViewController()
+        controller = self.setUpViews(for: controller)
+        controller.constraints = self.setUpConstraints(for: controller)
+        return controller
+    }
+    
+    private func setUpViews(for controller: AuthViewController) -> AuthViewController{
+        controller.view.backgroundColor = UIColor.white
+        
+        controller.emailLabel.text = "Login:"
+        controller.emailLabel.textAlignment = .center
+        controller.emailLabel.textColor = UIColor.black
+        controller.emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        controller.view.addSubview(controller.emailLabel)
+        
+        controller.passwordLabel.text = "Password:"
+        controller.passwordLabel.textAlignment = .center
+        controller.passwordLabel.textColor = UIColor.black
+        controller.passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        controller.view.addSubview(controller.passwordLabel)
+        
+        controller.emailField.text = "enter your email..."
+        controller.emailField.keyboardType = .emailAddress
+        controller.emailField.textContentType = .emailAddress
+        controller.emailField.translatesAutoresizingMaskIntoConstraints = false
+        controller.view.addSubview(controller.emailField)
+        
+        controller.passwordField.text = "enter your password..."
+        controller.passwordField.textContentType = .password
+        controller.passwordField.translatesAutoresizingMaskIntoConstraints = false
+        controller.view.addSubview(controller.passwordField)
+        
+        controller.signInButton.titleLabel?.text = "Login"
+        controller.view.addSubview(controller.signInButton)
+        
+        return controller
+    }
+    
+    private func setUpConstraints(for controller: AuthViewController) -> [NSLayoutConstraint] {
+        return [
+            controller.emailLabel.topAnchor.constraint(equalTo: controller.view.topAnchor, constant: 50.0),
+            controller.emailLabel.leadingAnchor.constraint(equalTo: controller.view.leadingAnchor, constant: 30.0),
+            controller.emailLabel.widthAnchor.constraint(equalToConstant: 50.0),
+            controller.emailLabel.heightAnchor.constraint(equalToConstant: 50.0),
+            
+            controller.emailField.topAnchor.constraint(equalTo: controller.view.topAnchor, constant: 50.0),
+            controller.emailField.leadingAnchor.constraint(equalTo: controller.emailLabel.trailingAnchor, constant: 20.0),
+            controller.emailField.trailingAnchor.constraint(equalTo: controller.view.trailingAnchor, constant: 30.0),
+            controller.emailField.heightAnchor.constraint(equalToConstant: 50.0),
+            
+            controller.passwordLabel.topAnchor.constraint(equalTo: controller.emailLabel.bottomAnchor, constant: 50.0),
+            controller.passwordLabel.leadingAnchor.constraint(equalTo: controller.view.leadingAnchor, constant: 30.0),
+            controller.passwordLabel.widthAnchor.constraint(equalToConstant: 50.0),
+            controller.passwordLabel.heightAnchor.constraint(equalToConstant: 50.0),
+            
+            controller.passwordField.topAnchor.constraint(equalTo: controller.emailField.bottomAnchor, constant: 50.0),
+            controller.passwordField.leadingAnchor.constraint(equalTo: controller.passwordLabel.trailingAnchor, constant: 20.0),
+            controller.passwordField.trailingAnchor.constraint(equalTo: controller.view.trailingAnchor, constant: 30.0),
+            controller.passwordField.heightAnchor.constraint(equalToConstant: 50.0),
+            
+            controller.signInButton.leadingAnchor.constraint(equalTo: controller.view.leadingAnchor, constant: 30.0),
+            controller.signInButton.trailingAnchor.constraint(equalTo: controller.view.trailingAnchor, constant: 30.0),
+            controller.signInButton.bottomAnchor.constraint(equalTo: controller.view.bottomAnchor, constant: 50.0),
+            controller.signInButton.heightAnchor.constraint(equalToConstant: 50.0)
+        ]
+    }
+
+}
--- Shop/Shop/App/BusinessLogic/Login Controller/AuthViewController.swift
+++ Shop/Shop/App/BusinessLogic/Login Controller/AuthViewController.swift
@@ -9,109 +9,49 @@
 import UIKit
 
 class AuthViewController: UIViewController {
-    private var emailField = UITextField()
-    private var emailLabel = UILabel()
+    var emailField = UITextField()
+    var emailLabel = UILabel()
     
-    private var passwordField = UITextField()
-    private var passwordLabel = UILabel()
+    var passwordField = UITextField()
+    var passwordLabel = UILabel()
     
-    private var signInButton = UIButton()
+    var signInButton = UIButton()
+    var constraints = [NSLayoutConstraint]()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated);
-        self.setUpViews()
-        self.setUpConstraints()
+        emailField.addTarget(self, action: #selector(startEditingField(_:)), for: .allTouchEvents)
+        passwordField.addTarget(self, action: #selector(startEditingField(_:)), for: .allTouchEvents)
+        signInButton.addTarget(self, action: #selector(tryLogin(_:)), for: .touchUpInside)
+        NSLayoutConstraint.activate(constraints)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension AuthViewController {
-    func setUpViews(){
-        self.view.backgroundColor = UIColor.white
-        
-        emailLabel.text = "Login:"
-        emailLabel.textAlignment = .center
-        emailLabel.textColor = UIColor.black
-        emailLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(emailLabel)
-        
-        passwordLabel.text = "Password:"
-        passwordLabel.textAlignment = .center
-        passwordLabel.textColor = UIColor.black
-        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(passwordLabel)
-        
-        emailField.text = "enter your email..."
-        emailField.keyboardType = .emailAddress
-        emailField.textContentType = .emailAddress
-        emailField.addTarget(self, action: #selector(startEditingField(_:)), for: .touchUpInside)
-        emailField.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(emailField)
-        
-        passwordField.text = "enter your password..."
-        passwordField.textContentType = .password
-        passwordField.addTarget(self, action: #selector(startEditingField(_:)), for: .touchUpInside)
-        passwordField.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(passwordField)
-        
-        signInButton.titleLabel?.text = "Login"
-        signInButton.addTarget(self, action: #selector(tryLogin(_:)), for: .touchUpInside)
-        self.view.addSubview(signInButton)
-    }
-    
     @objc func startEditingField(_ sender: UITextField){
         sender.text = nil
     }
     
     @objc func tryLogin(_ sender: AnyObject){
-        print("login initiated")
-    }
-    
-    func setUpConstraints() {
-        let constraints = [
-            emailLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50.0),
-            emailLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30.0),
-            emailLabel.widthAnchor.constraint(equalToConstant: 50.0),
-            emailLabel.heightAnchor.constraint(equalToConstant: 50.0),
-            
-            emailField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50.0),
-            emailField.leadingAnchor.constraint(equalTo: emailLabel.trailingAnchor, constant: 20.0),
-            emailField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 30.0),
-            emailField.heightAnchor.constraint(equalToConstant: 50.0),
-            
-            passwordLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 50.0),
-            passwordLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30.0),
-            passwordLabel.widthAnchor.constraint(equalToConstant: 50.0),
-            passwordLabel.heightAnchor.constraint(equalToConstant: 50.0),
-            
-            passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 50.0),
-            passwordField.leadingAnchor.constraint(equalTo: passwordLabel.trailingAnchor, constant: 20.0),
-            passwordField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 30.0),
-            passwordField.heightAnchor.constraint(equalToConstant: 50.0),
-            
-            signInButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30.0),
-            signInButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 30.0),
-            signInButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 50.0),
-            signInButton.heightAnchor.constraint(equalToConstant: 50.0)
-        ]
-        
-        NSLayoutConstraint.activate(constraints)
+        var authorize = Authorizator()
+        if let email = self.emailField.text{
+            if let pass = self.passwordField.text{
+                authorize.signIn(with: email, password: pass){ error in
+                    if error != nil {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        print("Success login")
+                        //Successful login attempt action
+                    }
+                }
+            }
+        }
     }
 }
