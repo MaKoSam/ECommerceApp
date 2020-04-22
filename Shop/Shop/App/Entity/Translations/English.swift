c0e6ca5a3dd9ca99be973d8302ab9face45e801f
MaKoSam <mazniker@icloud.com>
2020-04-22 15:58:25 +0800

    Изменил работу переводов, теперь на каждый язык свой класс, а Translations отвечает только за выборку одного языка

---
+++ Shop/Shop/App/Entity/Translations/English.swift
@@ -0,0 +1,49 @@
+//
+//  English.swift
+//  Shop
+//
+//  Created by Sam Mazniker on 22/04/2020.
+//  Copyright © 2020 Sam Mazniker. All rights reserved.
+//
+
+import UIKit
+
+class English {
+    static let shared = English()
+    private var dictionary = [String: String]()
+    
+    func getTranslations() -> [String: String]{
+        return dictionary
+    }
+    
+    private init() {
+        //Register & Login & User
+        dictionary.updateValue("Create your account", forKey: "signUpLabel")
+        dictionary.updateValue("Log into your account", forKey: "signInLabel")
+        dictionary.updateValue("Sign Up", forKey: "signUpBut")
+        dictionary.updateValue("Log In", forKey: "signInBut")
+        
+        dictionary.updateValue("Don't have an account?", forKey: "transferToSignUp")
+        dictionary.updateValue("Sign Up.", forKey: "transferToSignUpBut")
+        dictionary.updateValue("Already have account?", forKey: "transferToSignIn")
+        dictionary.updateValue("Sign In.", forKey: "transferToSignInBut")
+        
+        dictionary.updateValue("Your name", forKey: "name")
+        dictionary.updateValue("Email", forKey: "email")
+        dictionary.updateValue("Password", forKey: "password")
+        dictionary.updateValue("Password again", forKey: "passwordRep")
+        
+        //User address
+        dictionary.updateValue("Delivery Address", forKey: "address")
+        dictionary.updateValue("Country", forKey: "country")
+        dictionary.updateValue("City", forKey: "city")
+        dictionary.updateValue("Line one", forKey: "line1")
+        dictionary.updateValue("Line two", forKey: "line2")
+        dictionary.updateValue("House", forKey: "house")
+        dictionary.updateValue("Apartment", forKey: "apartment")
+        
+        //Function buttons
+        dictionary.updateValue("Update", forKey: "update")
+        dictionary.updateValue("Add to cart", forKey: "buy")
+    }
+}
--- Shop/Shop/App/Entity/Translations/Translations.swift
+++ Shop/Shop/App/Entity/Translations/Translations.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class Translations: NSObject {
-    private var russianTranslations = [String: String]()
-    private var englishTranslations = [String: String]()
-    
     func requestDictionary(for language: String) -> [String: String]{
         switch language {
-        case "ru":
-            return russianTranslations
-        case "en":
-            return englishTranslations
+        case "ru-RU":
+            return Russian.shared.getTranslations()
         default:
-            return englishTranslations
+            return English.shared.getTranslations()
         }
     }
     
@@ -28,69 +23,14 @@ class Translations: NSObject {
         print(systemLang)
         switch systemLang {
         case "ru":
-            return russianTranslations
+            return Russian.shared.getTranslations()
         case "ru-ru":
-            return russianTranslations
+            return Russian.shared.getTranslations()
         case "ru-RU":
-            return russianTranslations
+            return Russian.shared.getTranslations()
         default:
-            return englishTranslations
+            return English.shared.getTranslations()
         }
     }
     
-    override init(){
-        super.init()
-        russianTranslations.updateValue("Зарегистрируйтесь", forKey: "signUpLabel")
-        russianTranslations.updateValue("Войдите в свой аккаунт", forKey: "signInLabel")
-        russianTranslations.updateValue("Зарегистрироваться", forKey: "signUpBut")
-        russianTranslations.updateValue("Войти", forKey: "signInBut")
-        
-        russianTranslations.updateValue("У вас нет аккаунта?", forKey: "transferToSignUp")
-        russianTranslations.updateValue("Зарегистрируйтесь!", forKey: "transferToSignUpBut")
-        russianTranslations.updateValue("Уже есть аккаунт?", forKey: "transferToSignIn")
-        russianTranslations.updateValue("Войти.", forKey: "transferToSignInBut")
-        
-        russianTranslations.updateValue("Ваше имя", forKey: "name")
-        russianTranslations.updateValue("Ваша почта", forKey: "email")
-        russianTranslations.updateValue("Пароль", forKey: "password")
-        russianTranslations.updateValue("Пароль еще раз", forKey: "passwordRep")
-        
-        russianTranslations.updateValue("Адрес доставки", forKey: "address")
-        russianTranslations.updateValue("Страна", forKey: "country")
-        russianTranslations.updateValue("Город", forKey: "city")
-        russianTranslations.updateValue("Улица", forKey: "line1")
-        russianTranslations.updateValue("Дополнительно", forKey: "line2")
-        russianTranslations.updateValue("Дом", forKey: "house")
-        russianTranslations.updateValue("Квартира", forKey: "apartment")
-        
-        russianTranslations.updateValue("Обновить", forKey: "update")
-        russianTranslations.updateValue("В корзину", forKey: "buy")
-        
-        
-        englishTranslations.updateValue("Create your account", forKey: "signUpLabel")
-        englishTranslations.updateValue("Log into your account", forKey: "signInLabel")
-        englishTranslations.updateValue("Sign Up", forKey: "signUpBut")
-        englishTranslations.updateValue("Log In", forKey: "signInBut")
-        
-        englishTranslations.updateValue("Don't have an account?", forKey: "transferToSignUp")
-        englishTranslations.updateValue("Sign Up.", forKey: "transferToSignUpBut")
-        englishTranslations.updateValue("Already have account?", forKey: "transferToSignIn")
-        englishTranslations.updateValue("Sign In.", forKey: "transferToSignInBut")
-        
-        englishTranslations.updateValue("Your name", forKey: "name")
-        englishTranslations.updateValue("Email", forKey: "email")
-        englishTranslations.updateValue("Password", forKey: "password")
-        englishTranslations.updateValue("Password again", forKey: "passwordRep")
-        
-        englishTranslations.updateValue("Delivery Address", forKey: "address")
-        englishTranslations.updateValue("Country", forKey: "country")
-        englishTranslations.updateValue("City", forKey: "city")
-        englishTranslations.updateValue("Line one", forKey: "line1")
-        englishTranslations.updateValue("Line two", forKey: "line2")
-        englishTranslations.updateValue("House", forKey: "house")
-        englishTranslations.updateValue("Apartment", forKey: "apartment")
-        
-        englishTranslations.updateValue("Update", forKey: "update")
-        englishTranslations.updateValue("Add to cart", forKey: "buy")
-    }
 }
